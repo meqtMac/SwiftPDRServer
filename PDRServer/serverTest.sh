281c58ef7923474251e61dfaa0e2468dd334fb14
meqt <meqt@foxmail.com>
2023-02-22 18:23:59 +0800

    serverTest now runs

--- PDRServer/serverTest.sh
+++ PDRServer/serverTest.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/zsh
 swift run Run routes
 vapor run migrate --revert
 vapor run migrate
 testpath="testpath"$(date)
-mkdir $testpath
+mkdir "$testpath"
 port=8888
 PID=$(lsof -t -i:$port )
 if [ -z "$PID" ]; then
@@ -16,15 +15,20 @@ vapor run serve --port $port > "$testpath"/vapor_log_8888.txt &
 
 sleep 5
 
-for path in [runnings, positions, runnings/pdr]
+for path in runnings positions 
 do
-    for batch in [27, 28, 29]
+    for batch in 27 28 29
     do
-        curl "http://localhost:$port/$path\?batch\=$batch" > "$testpath"/"$path$batch.json"
+        curl "http://localhost:$port/$path?batch=$batch" > "$testpath"/"$path$batch.json"
     done
 done
 
-curl "http://localhost:$port/runnings/train\?k\=0.4\&m\=0.08\&dk\=0.01\&dm\=0.001\&eta\=0.000002\&epochs\=1000"
+for batch in 27 28 29
+do
+    curl "http://localhost:$port/runnings/pdr?batch=$batch" > "$testpath"/"pdr$batch.json"
+done
+
+curl "http://localhost:$port/runnings/train?k=0.4&m=0.08&dk=0.01&dm=0.001&eta=0.000002&epochs=200"
 
 PID=$(lsof -t -i:$port)
 if [ -z "$PID" ]; then
